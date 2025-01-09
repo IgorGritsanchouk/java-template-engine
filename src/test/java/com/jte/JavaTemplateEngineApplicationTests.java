1260a26e2a6085278c361c8df3440b4fa186a28a
igor <igor.gritsanchouk@gmail.com>
2025-01-09 09:54:31 +0000

    First commit

---
+++ src/test/java/com/jte/JavaTemplateEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class JavaTemplateEngineApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
